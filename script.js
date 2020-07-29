b077274b41c58bd76db78cb8c80f1a180e98d6ad
Alan <alanhood77@gmail.com>
2020-07-29 21:06:43 +0100

    image footer and icons added

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
-const apiKey = kBBx87b9TG72Z8mvtPjX_mmUsJF-bBRXToswlUK014M;
-const secretKey = fqi4RVX09KR97-4N-Qgl67BE8GXeh2P9ka264xMVJcI;
+const apiKey = `kBBx87b9TG72Z8mvtPjX_mmUsJF-bBRXToswlUK014M`;
+
 
 const count = 10;
 
 const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+
+// fetch pics from Unsplash
+
+async function getPhotos() {
+    try {
+        const response = await fetch(apiUrl);
+        const data = await response.json();
+        console.log(data);        
+    } catch (error) {
+        // catch error here
+    }
+}
+
+// on load
+getPhotos();
