29fd2560c1d184ec545fc473ef305ef880f38f9d
Alan <alanhood77@gmail.com>
2020-07-29 18:57:38 +0100

    basic html and styling complete

---
+++ script.js
@@ -0,0 +1,6 @@
+const apiKey = kBBx87b9TG72Z8mvtPjX_mmUsJF-bBRXToswlUK014M;
+const secretKey = fqi4RVX09KR97-4N-Qgl67BE8GXeh2P9ka264xMVJcI;
+
+const count = 10;
+
+const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
