947abc1e64ef935df704682ce1276a6324bfb2d5
Alan <alanhood77@gmail.com>
2020-07-31 22:05:23 +0100

    infinite scroll and final styling added

--- script.js
+++ script.js
@@ -4,27 +4,70 @@ const button = document.getElementById('twitter-btn')
 const loader = document.getElementById('loader');
 
 
-
+let ready = false;
+let imagesLoaded = 0;
+let totalImages = 0;
 let photosArray = [];
 
 const apiKey = `h_Pi7YDOzj6t-CSxJHHrpqn3eMNfxbhXnZbigskBr7U`;
-const count = 10;
+const count = 30;
 const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+
+
+// Check if all images were loaded
+function imageLoaded() {
+    imagesLoaded++;
+    console.log(imagesLoaded);
+    if (imagesLoaded === totalImages) {
+        ready = true;
+        loader.hidden = true;
+        console.log('ready =', ready);
+        
+    }
+    
+}
+
+// Helper function to Set Attributes on DOM elements
+function setAttributes(element, attributes) {
+    // assign key const(href, src, etc) in atrributes
+    for (const key in attributes) {
+        element.setAttribute(key, attributes[key])
+    }
+}
 
 // Create elements for Links and Photos, Add to DOM
 function displayPhotos() {
+    imagesLoaded = 0;
+    totalImages = photosArray.length;
+    console.log('total images', totalImages);
+    
     // run function for each object 
     photosArray.forEach((photo) => {
         //  create <a> to link to Unsplash
         const item = document.createElement('a');
-        item.setAttribute('href', photo.links.html);
-        item.setAttribute('target', '_blank');
+        // item.setAttribute('href', photo.links.html);
+        // item.setAttribute('target', '_blank');
+        setAttributes(item, {
+            href: photo.links.html,
+            target: '_blank'
+        });
+
+
         // Create <img> for photo
         const img = document.createElement('img');
         img.id = "unsplash-image"
-        img.setAttribute('src', photo.urls.regular);
-        img.setAttribute('alt', photo.alt_description);
-        img.setAttribute('title', photo.alt_description);
+        // img.setAttribute('src', photo.urls.regular);
+        // img.setAttribute('alt', photo.alt_description);
+        // img.setAttribute('title', photo.alt_description);
+        setAttributes(img, {
+            src: photo.urls.regular,
+            alt: photo.alt_description,
+            title: photo.alt_description
+        });
+
+        // Event Listener, check when each is finished loading
+        img.addEventListener('load', imageLoaded)
+
         // Put <img> inside <a>, then put both inside imageContainer element
         item.appendChild(img);
         imageContainer.appendChild(item);
@@ -118,12 +161,23 @@ async function getPhotos() {
 // Event Delegation
 document.querySelector('body').addEventListener('click', function (e) {
     const image = document.getElementById('unsplash-image')
-    const selectedImage = image.innerHTML
+    const selectedImage = image.src
     
     const twitterURL = `https://twitter.com/intent/tweet?url=${selectedImage}`;
     if (e.target.id === 'twitter-btn') {
         window.open(twitterURL, '_blank')
     } 
+})
+
+// check to see if scrolling near bottom of page, Load More Pics
+window.addEventListener('scroll', () => {
+    if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000 && ready) {
+        ready = false;
+        getPhotos();
+        
+        
+    }
+    
 })
 
 // on load
